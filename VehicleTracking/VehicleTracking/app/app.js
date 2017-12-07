3c81e1445a6e98e35a26366eb1bb612f395fa7d4
dnsuresh <32706072+dnsuresh@users.noreply.github.com>
2017-12-07 18:46:30 +0530

    Vehicle tracking
    Vehicle tracking

---
+++ VehicleTracking/VehicleTracking/app/app.js
@@ -0,0 +1,9 @@
+﻿var app = angular.module("trackingApp", ["ui.router"]);
+app.config(function ($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise('/');
+    $stateProvider
+    .state('home', {
+        url: "/",
+        templateUrl: "app/view/home.html"
+    })
+});
---
+++ VehicleTracking/VehicleTracking/app/controller/home-ctrl.js
@@ -0,0 +1,35 @@
+﻿app.controller("homeCtrl", ['$scope', '$http', function ($scope, $http, $stateParams, $state) {
+    $scope.showModal = false;
+    $scope.buttonClicked = "";
+    $scope.assets = ['Asset1', 'Asset2'];
+    $scope.toggleModal = function (btnClicked) {
+        $scope.buttonClicked = btnClicked;
+        $scope.showModal = !$scope.showModal;
+    };
+    $scope.JobDetails = [
+        {
+            JobID:1,
+            JobDescrption: "JobDescrption1",
+            Distancetravelled: "25Kms",
+            Duration:"25"
+        },
+        {
+            JobID: 2,
+            JobDescrption: "JobDescrption2",
+            Distancetravelled: "15Kms",
+            Duration: "15"
+        },
+        {
+            JobID: 3,
+            JobDescrption: "JobDescrption3",
+            Distancetravelled: "10Kms",
+            Duration: "10"
+        },
+        {
+            JobID: 4,
+            JobDescrption: "JobDescrption4",
+            Distancetravelled: "9Kms",
+            Duration: "9"
+        }
+    ];
+}]);    
