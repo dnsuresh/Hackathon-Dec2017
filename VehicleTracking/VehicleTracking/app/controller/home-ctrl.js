a0c769c9bee600475532b745ef7ec59223e9d85f
dnsuresh <32706072+dnsuresh@users.noreply.github.com>
2017-12-07 22:28:24 +0530

    Vehicle tracking changes
    Vehicle tracking changes

--- VehicleTracking/VehicleTracking/app/controller/home-ctrl.js
+++ VehicleTracking/VehicleTracking/app/controller/home-ctrl.js
@@ -1,7 +1,9 @@
 ﻿app.controller("homeCtrl", ['$scope', '$http', function ($scope, $http, $stateParams, $state) {
     $scope.showModal = false;
     $scope.buttonClicked = "";
-    $scope.assets = ['Asset1', 'Asset2'];
+    $scope.assets = [
+        'Asset1', 'Asset2', 'Asset3', 'Asset4', 'Asset5', 'Asset6', 'Asset7', 'Asset8', 'Asset9', 'Asset10', 'Asset11', 'Asset12', 'Asset13', 'Asset14', 'Asset15', 'Asset16', 'Asset17', 'Asset18', 'Asset19', 'Asset20', 'Asset21', 'Asset22'
+    ];
     $scope.toggleModal = function (btnClicked) {
         $scope.buttonClicked = btnClicked;
         $scope.showModal = !$scope.showModal;
