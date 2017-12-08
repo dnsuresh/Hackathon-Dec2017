91b0ed92ff3e828179be823902ac7efd38a6a0fc
dnsuresh <32706072+dnsuresh@users.noreply.github.com>
2017-12-08 17:32:20 +0530

    API call changes
    API call changes

--- VehicleTracking/VehicleTracking/app/controller/home-ctrl.js
+++ VehicleTracking/VehicleTracking/app/controller/home-ctrl.js
@@ -1,37 +1,30 @@
 ﻿app.controller("homeCtrl", ['$scope', '$http', function ($scope, $http, $stateParams, $state) {
     $scope.showModal = false;
     $scope.buttonClicked = "";
-    $scope.assets = [
-        'Asset1', 'Asset2', 'Asset3', 'Asset4', 'Asset5', 'Asset6', 'Asset7', 'Asset8', 'Asset9', 'Asset10', 'Asset11', 'Asset12', 'Asset13', 'Asset14', 'Asset15', 'Asset16', 'Asset17', 'Asset18', 'Asset19', 'Asset20', 'Asset21', 'Asset22'
-    ];
-    $scope.toggleModal = function (btnClicked) {
-        $scope.buttonClicked = btnClicked;
+    getAssets();
+    $scope.toggleModal = function (asset) {
+        $scope.buttonClicked = asset.AssetName;
         $scope.showModal = !$scope.showModal;
+        getJobDetails(asset.AssetID);
     };
-    $scope.JobDetails = [
-        {
-            JobID:1,
-            JobDescrption: "JobDescrption1",
-            Distancetravelled: "25Kms",
-            Duration:"25"
-        },
-        {
-            JobID: 2,
-            JobDescrption: "JobDescrption2",
-            Distancetravelled: "15Kms",
-            Duration: "15"
-        },
-        {
-            JobID: 3,
-            JobDescrption: "JobDescrption3",
-            Distancetravelled: "10Kms",
-            Duration: "10"
-        },
-        {
-            JobID: 4,
-            JobDescrption: "JobDescrption4",
-            Distancetravelled: "9Kms",
-            Duration: "9"
-        }
-    ];
+    function getAssets() {
+        var url = "http://localhost/VehicleTrackingWebAPI/Assets";
+        $http.get(url)
+        .then(function (response) {
+            $scope.assets = response.data;
+        })
+        .catch(function (response) {
+            alert('failure');
+        });
+    }
+    function getJobDetails(assetid) {
+        var url = "http://localhost/VehicleTrackingWebAPI/JobDetails?AssetID=1";
+        $http.get(url)
+        .then(function (response) {
+            $scope.JobDetails = response.data;
+        })
+        .catch(function (response) {
+            alert('failure');
+        });
+    }
 }]);    
