5cee4e22d602f63478979485c0e57c33f77e048c
dnsuresh <32706072+dnsuresh@users.noreply.github.com>
2018-02-05 21:33:39 +0530

    asset plotting changes

--- VehicleTracking/VehicleTracking/app/controller/home-ctrl.js
+++ VehicleTracking/VehicleTracking/app/controller/home-ctrl.js
@@ -2,6 +2,14 @@
     $scope.showModal = false;
     $scope.buttonClicked = "";
     getAssets();
+    $scope.assetClicked = function () {
+        debugger;
+        var a = angular.module('trackingApp', []);
+        var frameEle = document.getElementById("myIframe").contentWindow;
+        frameEle.postMessage('{"command":"assetsAdded","payload":[{"id":"89764","heading":"S","location":{"lat":33.346069,"lon":-111.95575},"text":"TabAFunny","toolTipText":"","colorHex":"#00FF00","hasOrder":false,"voiceEnabled":false,"hasTicket":false,"eventStatusCode":null,"eventColorHex":"#000000","voiceStatus":null}]}', '*');
+        frameEle.postMessage('{"command":"assetsModified","payload":[{"id":"89764","heading":"N","location":{"lat":33.346069,"lon":-111.95575},"text":"TabAFunny","toolTipText":"<font size=&#34;+1&#34;><u><b>Asset:</b></u> TabAFunny</font><br><b>Status:</b> 30 Mph N -12/8/2017 10:50:00 AM","colorHex":"#00FF00","hasOrder":false,"voiceEnabled":false,"hasTicket":false,"eventStatusCode":null,"eventColorHex":"#000000","voiceStatus":null}]}', '*');
+        frameEle.postMessage('{"command":"zoomToExtent","payload":[{"lat":33.346069,"lon":-111.955749},{"lat":33.346069,"lon":-111.955749}]}', '*');
+    };
     $scope.toggleModal = function (asset) {
         $scope.buttonClicked = asset.AssetName;
         $scope.showModal = !$scope.showModal;
